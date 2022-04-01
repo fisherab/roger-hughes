7d595662f1575bb36a56ef32111d9e195af9e19c
Steve Fisher <dr.s.m.fisher@gmail.com>
2022-04-01 10:50:13 +0100

    Scan over different x values

--- ngs.py
+++ ngs.py
@@ -3,7 +3,6 @@ from datetime import date
 from glob import glob
 import sys
 
-fifty = 46.0
 year = date.today().year
 mps=0
 percentage=1
@@ -23,7 +22,6 @@ def getData(year, headingsWanted):
                 
                 fieldPositions = []
                 width = len(row)
-  #              print (row)
                 i = 0
                 for heading in row:
                     if heading in headingsWanted:
@@ -44,8 +42,7 @@ def getData(year, headingsWanted):
                         key = row[i]
                     else:
                         record.append(row[i])
-   #             if n < 10:
-   #                 print(key, record)
+ #               if n < 10: print(key, record)
                 members[key] = record
             n += 1
     return members
@@ -58,33 +55,32 @@ def floatAny(arg):
         return 0
     
 thisYear = getData(year,['National number', 'EBU rank', 'NGS grade','First name', 'Last name'])
-lastYear = getData(year-1,['EBU number', 'EBU rank', 'NGS grade','First name', 'Last name'])
+lastYear = getData(year-1,['National number', 'EBU rank', 'NGS grade','First name', 'Last name'])
 previousYear = getData(year-2,['EBU number', 'EBU rank', 'NGS grade','First name', 'Last name'])
-
-qualifiers = []
-
-    
-
-for member in thisYear:
-    if member in lastYear and floatAny(thisYear[member][percentage]) > floatAny(lastYear[member][percentage]) and floatAny(lastYear[member][percentage]) > 0:
-        if floatAny(thisYear[member][percentage]) > fifty and  floatAny(lastYear[member][percentage]) < fifty:
-            if member in previousYear and floatAny(previousYear[member][percentage]) < fifty:
-                if lastYear[member][mps] in ["Local Master", "Club Master", "Area Master", "District Master", "CountyMaster", "Not public on ebu.co.uk"]:
-
-                    improvement = round(floatAny(thisYear[member][percentage]) - floatAny(lastYear[member][percentage]),2)
-                    person = thisYear[member]
-                    name = person[2] + " " + person[3]
-                    qualifiers.append(( member, name, previousYear[member][mps], person[mps] , previousYear[member][percentage], lastYear[member][percentage], person[percentage],
-                                  improvement))
- 
-      
-qualifiers.sort(key=lambda people: people[7], reverse=True)
 
 with open('results.csv','w', newline='') as csvfile:
     w = csv.writer(csvfile)
-    w.writerow(['EBU num', 'Name', 'Last year MPs', 'This year MPs', 'NGS 2 years ago', 'NGS 1 Year ago', 'NGS Now', 'Improvement'])
-    for q in qualifiers:
-        print (q)
-        w.writerow(q)
+    w.writerow(['EBU num', 'Name', 'Last year MPs', 'This year MPs', 'NGS 2 years ago', 'NGS 1 Year ago', 'NGS Now', 'Improvement', '"Fifty"'])
+    for fifty in range(45,51): 
+
+        qualifiers = []
+
+        for member in thisYear:
+            if member in lastYear and floatAny(thisYear[member][percentage]) > floatAny(lastYear[member][percentage]) and floatAny(lastYear[member][percentage]) > 0:
+                if floatAny(thisYear[member][percentage]) > fifty and  floatAny(lastYear[member][percentage]) < fifty:
+                    if member in previousYear and floatAny(previousYear[member][percentage]) < fifty:
+                        if lastYear[member][mps] in ["Local Master", "Club Master", "Area Master", "District Master", "CountyMaster", "Not public on ebu.co.uk"]:
+
+                            improvement = round(floatAny(thisYear[member][percentage]) - floatAny(lastYear[member][percentage]),2)
+                            person = thisYear[member]
+                            name = person[2] + " " + person[3]
+                            qualifiers.append(( member, name, previousYear[member][mps], person[mps] , previousYear[member][percentage], lastYear[member][percentage], person[percentage],
+                                          improvement, fifty))
+                    
+        qualifiers.sort(key=lambda people: people[7], reverse=True)
+   
+        for q in qualifiers:
+            print (q)
+            w.writerow(q)
  
  
